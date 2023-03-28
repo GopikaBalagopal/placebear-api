b30c20217797c8c4c85fc4d7a96166bed5024c01
GopikaBalagopal <kvgopikabalagopal@gmail.comm>
2023-03-28 18:43:48 +0530

    placebear

---
+++ script.js
@@ -0,0 +1,13 @@
+let width=prompt("enter the pics width");
+let height=prompt("enter the pics height");
+
+placebear()
+
+async function placebear(){
+    let link=`https://placebear.com/${width}/${height}`
+    let img=document.createElement("img");
+    let main=document.getElementById("image");
+    img.src=link;
+    main.append(img);
+    //document.getElementById("image").innerHTML=`<img src=${link} alt="dog pics"title="dog"/>`
+}
